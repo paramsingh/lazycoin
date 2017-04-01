9e0934c52e1dbd9b1364e40baab156f4496f7cf8
shikharsrivastava <shikharsri1996@gmail.com>
2017-04-01 22:59:59 +0530

    Added threads for send and receive

--- client.py
+++ client.py
@@ -1,14 +1,52 @@
 import socket
 import funcs
+import user
+import threading
+import json
 
 HOST = '127.0.0.1'
 PORT = 9997
 
+
+def handle_send(sock, User):
+	pass
+
+''' Thread receives broadcasted data'''
+def handle_receive(sock, User):
+
+	#receiving
+	while True:
+		message = json.loads(funcs.receive_message(sock))
+		if message['type'] == 'Transaction':
+			# it is a transaction
+		else
+			# it is a block request
+
+
+
+
+
+
+
 if __name__ == '__main__':
+	
+	# the user managing this miner
+	User = LazyUser()
+
+	# boradcasting connection to csec
 	clientSock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 	clientSock.connect((HOST,PORT))
 
-	funcs.send_message(clientSock, "Princu")
+	# The receiving thread for this miner
+	th = threading.Thread(target = handle_receive, args = [clientSock,User], daemon = True)
+	th.start()
+
+	#The Broadcasting thread for this miner
+	th = threading.Thread(target = handle_send, args = [clientSock,User], daemon = True)
+	th.start()
+
+
+	'''funcs.send_message(clientSock, "Princu")
 
 	while(True):
 		message = funcs.receive_message(clientSock)
@@ -16,6 +54,6 @@ if __name__ == '__main__':
 
 	while True: 
 		pass
-	clientSock.close()
+	clientSock.close()'''
 
 
